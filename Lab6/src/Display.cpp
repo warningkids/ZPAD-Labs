57e9089c57c335df58922ca7cead3567cec0b745
warningkids <vladench16@gmail.com>
2026-04-07 07:04:29 -0400

    Lab7

--- Lab6/src/Display.cpp
+++
@@ -1,9 +0,0 @@
-#include "Display.hpp"
-
-Display::Display() {
-    cv::namedWindow(windowName, cv::WINDOW_NORMAL);
-}
-
-void Display::show(const cv::Mat& frame) {
-    cv::imshow(windowName, frame);
-}
